3995da8ed870de2b3237386e1561d67b8bec30a2
Irfan Shaikh <irfan.shaikh@adapty.com>
2018-05-02 19:00:54 +0530

    completed 02/05/18

--- irf-02-05-18/main.js
+++ irf-02-05-18/main.js
@@ -29,7 +29,7 @@ function logFunction() {
 }
 
 new logFunction(); // logs logFunction {}
-*/
+
  // Saves a NodeList of all list items on the page in listItems
  var listItems = document.querySelectorAll('ul li');
  // Loops through each of the Node in the listItems NodeList and logs its content
@@ -38,3 +38,63 @@ new logFunction(); // logs logFunction {}
      console.log(this.innerHTML);
    }).call(listItems[i]);
  }
+
+
+function a() {
+  let a = 10
+  function b() {
+    console.log(a)
+  }
+}
+a();
+
+const person = { name: "Irfan" }
+
+let greeting = function(a, b) {
+  console.log(a + " " + b + " " + this.name)
+}
+
+greeting.call(person, "Hello", "Bro")
+
+
+
+const person = { name: "Rakshith" }
+
+let greeting = function(a,b) {
+  console.log(a + " "+ b + " " + this.name)
+}
+
+greeting.apply(person, ["Hello","Bro"])
+
+
+
+const person = {
+  name: "Irfan",
+  age: 24
+}
+
+let greeting = function(a, b) {
+  console.log(a + " " + b + " " + this.name)
+  console.log(this.age)
+}
+
+let bound = greeting.bind(person)
+
+bound("Hello", "the Greatest")
+*/
+
+// function ab(c, d) {
+//   console.log("hey" + " " + c + " " + d)
+// }
+
+// ab("Hello", "bro")
+
+const person = {
+  name: "shivani",
+  age: 36,
+  method: function() {
+    console.log(this.age);
+  }
+}
+
+person.method()
