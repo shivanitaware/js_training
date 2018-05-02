3025705bf2639e2c701ea0af9b4e9d3b761ec01e
Irfan Shaikh <irfan.shaikh@adapty.com>
2018-05-02 12:56:13 +0530

    practice 1

---
+++ irf-02-05-18/main.js
@@ -0,0 +1,10 @@
+function outerFunction () {
+    const outer = `I see the outer variable!`
+  
+    return function innerFunction() {
+      console.log(outer)
+    }
+  }
+  
+let outer = outerFunction() // I see the outer variable!
+console.log(outer())
