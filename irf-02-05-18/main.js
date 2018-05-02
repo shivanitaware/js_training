c5478643bb47c2f4c7e34f9fe7de96441b6a2b3b
Irfan Shaikh <irfan.shaikh@adapty.com>
2018-05-02 19:00:17 +0530

    Scope and closure

--- irf-02-05-18/main.js
+++ irf-02-05-18/main.js
@@ -9,5 +9,32 @@ function outerFunction () {
   
 let outer = outerFunction() // I see the outer variable!
 console.log(outer())
-*/
 
+
+function secret () {
+  let secretCode = "I'm greatest";
+  return {
+    saySecretCode: () => {
+      console.log(secretCode)
+    }
+  }
+}
+
+const theSecret = secret()
+theSecret.saySecretCode()
+
+
+function logFunction() {
+  console.log(this);
+}
+
+new logFunction(); // logs logFunction {}
+*/
+ // Saves a NodeList of all list items on the page in listItems
+ var listItems = document.querySelectorAll('ul li');
+ // Loops through each of the Node in the listItems NodeList and logs its content
+ for (var i = 0; i < listItems.length; i++) {
+   (function () {
+     console.log(this.innerHTML);
+   }).call(listItems[i]);
+ }
